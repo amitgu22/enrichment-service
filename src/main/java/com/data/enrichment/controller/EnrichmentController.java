6facb28bada6168217d33fb634cbc653a7bf8c14
amitgu22 <guravamit22@gmail.com>
2022-07-28 12:19:40 +0530

    Add files via upload

--- src/main/java/com/data/enrichment/controller/EnrichmentController.java
+++ src/main/java/com/data/enrichment/controller/EnrichmentController.java
@@ -1,13 +1,13 @@
 package com.data.enrichment.controller;
 
+import com.data.enrichment.exception.InvalidRequestException;
+import com.data.enrichment.exception.ErrorCode;
 import com.data.enrichment.kafka.input.InputProducer;
 import com.data.enrichment.model.Population;
 import com.data.enrichment.service.impl.EnrichmentServiceImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -31,17 +31,17 @@ public class EnrichmentController {
 
     public EnrichmentController(InputProducer kafkaProducer, EnrichmentServiceImpl enrichmentService) {
         this.kafkaProducer = kafkaProducer;
-
         this.enrichmentService = enrichmentService;
     }
 
 
     @RequestMapping(method = RequestMethod.POST, value = "/api/sendRequest")
-    public ResponseEntity<String> sendMessage(@RequestBody Population population) {
-        logger.info("Sending Message : " + population.toString());
-
+    public ResponseEntity<String> sendMessage(@RequestBody Population population) throws InvalidRequestException {
+        logger.info(" Message Received : " + population.toString());
+        if(population.getCity() == null || population.getCity().isEmpty()){
+            throw new InvalidRequestException("Validation failed for incoming request with error message {}" + ErrorCode.INVALID_REQUEST);
+        }
         this.kafkaProducer.sendMessage(population);
-
         return ResponseEntity.ok(status);
 
 
